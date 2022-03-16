bdc660a06e046589106c6139a790d0cad3a81e9f
Yagnesh Modh <hey@yagneshmodh.com>
2022-03-16 17:58:37 +0530

    completed basic todo app

--- src/Pages/Todo/index.jsx
+++ src/Pages/Todo/index.jsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React, { PureComponent, createRef } from 'react';
 
 class Todo extends PureComponent {
@@ -6,6 +7,7 @@ class Todo extends PureComponent {
     this.todoTextRef = createRef();
     this.state = {
       todoList: [],
+      filterType: 'all',
     };
   }
 
@@ -14,7 +16,11 @@ class Todo extends PureComponent {
     const todoText = this.todoTextRef.current.value;
     this.setState(
       ({ todoList }) => ({
-        todoList: [todoText, ...todoList],
+        todoList: [
+          { id: new Date().valueOf(), text: todoText, isDone: false },
+          ...todoList,
+        ],
+        filterType: 'all',
       }),
       () => {
         this.todoTextRef.current.value = '';
@@ -22,19 +28,111 @@ class Todo extends PureComponent {
     );
   };
 
+  toggleComplete = (item) => {
+    this.setState(({ todoList }) => {
+      // O(logN)
+      const index = todoList.findIndex((x) => x.id === item.id);
+      return {
+        todoList: [
+          ...todoList.slice(0, index),
+          { ...item, isDone: !item.isDone },
+          ...todoList.slice(index + 1),
+        ],
+      };
+    });
+  };
+
+  deleteTodo = (item) => {
+    this.setState(({ todoList }) => {
+      const index = todoList.findIndex((x) => x.id === item.id);
+      return {
+        todoList: [...todoList.slice(0, index), ...todoList.slice(index + 1)],
+      };
+    });
+  };
+
+  filterTodo = (filterType) => {
+    this.setState({
+      filterType,
+    });
+  };
+
   render() {
-    const { todoList } = this.state;
+    const { todoList, filterType } = this.state;
     return (
-      <div>
-        <h1>Todo App</h1>
-        <form onSubmit={this.addTodo}>
-          <input type="text" ref={this.todoTextRef} />
-          <button type="submit">Add Todo</button>
+      <div className="flex flex-col items-center h-screen">
+        <h1 className="text-2xl font-bold my-6 ">Todo App</h1>
+        <form onSubmit={this.addTodo} className="flex">
+          <input
+            type="text"
+            className="rounded-l-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            ref={this.todoTextRef}
+          />
+          <button
+            type="submit"
+            className="px-4 bg-blue-500 rounded-r-md text-white"
+          >
+            Add Todo
+          </button>
         </form>
-        <div>
-          {[...new Set(todoList)].map((item) => (
-            <div key={item}>{item}</div>
-          ))}
+        <div className="w-full flex-1 overflow-y-auto my-4">
+          {todoList
+            .filter((item) => {
+              switch (filterType) {
+                case 'completed':
+                  return item.isDone === true;
+                case 'pending':
+                  return item.isDone === false;
+                default:
+                  return true;
+              }
+            })
+            .map((item) => (
+              <div key={item.id} className="m-4 flex items-center">
+                <input
+                  type="checkbox"
+                  checked={item.isDone}
+                  onChange={() => this.toggleComplete(item)}
+                />
+                <p
+                  className={classNames('px-4 flex-1 text-xl', {
+                    'line-through': item.isDone,
+                  })}
+                >
+                  {item.text}
+                </p>
+                <button
+                  type="button"
+                  className="px-4 py-2 bg-blue-500 rounded-md text-white"
+                  onClick={() => this.deleteTodo(item)}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+        </div>
+        <div className="w-full flex">
+          <button
+            type="button"
+            onClick={() => this.filterTodo('all')}
+            className="flex-1 px-4 py-2 bg-blue-500 text-white"
+          >
+            All
+          </button>
+          <button
+            type="button"
+            onClick={() => this.filterTodo('pending')}
+            className="flex-1 px-4 py-2 bg-blue-500 text-white"
+          >
+            Pending
+          </button>
+          <button
+            type="button"
+            onClick={() => this.filterTodo('completed')}
+            className="flex-1 px-4 py-2 bg-blue-500 text-white"
+          >
+            Completed
+          </button>
         </div>
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
     ],
   },
   plugins: [
